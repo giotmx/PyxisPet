b1659d5305dcfdefc91f0329777841b11a184f5a
Giovanna Terni Malvestiti <giovannaterni@yahoo.com.br>
2020-05-12 00:35:21 -0300

    Base Do Site

---
+++ header.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="zxx">
+<head>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- font awesome CSS -->
+    <link rel="stylesheet" href="css/all.css">
+    <!-- style CSS -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
